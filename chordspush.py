6b632a7b82d04b99f1ef59400e829856474b59ca
Jeaime Powell <jeaimehp@gmail.com>
2019-01-04 16:17:24 -0600

    Added all sensors to chords push

--- chordspush.py
+++ chordspush.py
@@ -13,6 +13,23 @@ import datetime
 #Libray to post to CHoRDS
 import requests
 
+# EnviroPhat 
+from envirophat import leds, motion, light, weather, analog
+
+## Envirophat Units
+unit = 'Pa' # Pressure unit, can be either hPa (hectopascals) or Pa (pascals)
+
+## Envirophat Analog TMP36 Pins
+cm0=0
+cm5=1
+cm10=2
+
+##Generate Ouput
+point0_C=(analog.read(cm0)-.5)*100
+point1_C=(analog.read(cm5)-.5)*100
+point2_C=(analog.read(cm10)-.5)*100
+
+
 #Libraries for the adafruit_si7021
 import board
 import busio
@@ -31,9 +48,24 @@ url = "http://chords.tacc.cloud/measurements/url_create?"
 #Initiate Dictionary
 chordssub = {"instrument_id" : 5}
 chordssub = {"sensor_id" : 7}
+
 #Add variables from sensors to dictionary
+# si7021 data
 chordssub["temp"] = sensor.temperature
 chordssub["relhumidity"] = sensor.relative_humidity
+# Envirophat data
+chordssub["light"] = light.light()
+chordssub["tempout"] = weather.temperature()
+chordssub["pressure"] = weather.pressure(unit=unit)
+chordssub["tempat0cm"] = point0_C
+chordssub["tempat5cm"] = point1_C
+chordssub["tempat10cm"] = point2_C
+
+# Relay Boolean 
+
+
+
+
 
 #Display added values
 print (chordssub)
